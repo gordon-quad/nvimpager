485acb81ae6807c50b994534703eabf7d920c5a9
Lucas Hoffmann <l-m-h@web.de>
2018-08-08 00:38:52 +0200

    fix for osx again

--- xxx.sh
+++ xxx.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 printf 1:
 ps -o pid=,comm=,args= $$
@@ -7,6 +7,6 @@ ps -o pid=,comm=,args= $(ps -o ppid= $$)
 printf 2:
 ps -o pid=,comm=,args= $(ps -o ppid= $(ps -o ppid= $$))
 
-ps -o pid,ppid,comm,cmd
+ps -o pid,ppid,comm,args
 
 nvim --cmd "echo nvim_get_proc($$)" --cmd q
