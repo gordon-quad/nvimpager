fd5ec97f61323abaaeb4dd8425489524d7d98684
Lucas Hoffmann <l-m-h@web.de>
2018-08-08 00:32:29 +0200

    fix for osx

--- xxx.sh
+++ xxx.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 printf 1:
-ps -o pid=,comm=,cmd= $$
+ps -o pid=,comm=,args= $$
 printf 2:
-ps -o pid=,comm=,cmd= $(ps -o ppid= $$)
+ps -o pid=,comm=,args= $(ps -o ppid= $$)
 printf 2:
-ps -o pid=,comm=,cmd= $(ps -o ppid= $(ps -o ppid= $$))
+ps -o pid=,comm=,args= $(ps -o ppid= $(ps -o ppid= $$))
 
 ps -o pid,ppid,comm,cmd
 
